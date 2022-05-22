245e7c5e96516f690f9ba39096075f28e23f3c54
Abdullah Al Masud <masudctg00@gmail.com>
2022-05-22 23:14:04 +0600

    reviews api written

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.02fsy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const partCollection = client.db('InsidePC').collection('parts');
+        const reviewCollection = client.db('InsidePC').collection('reviews');
+
+        // GET parts from db
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = partCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts)
+        })
+
+        // GET reviews from db
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello From Inside PC')
