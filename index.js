68d8121bb0a0315ca0666ce1fe2d1882942d9e9a
Abdullah Al Masud <masudctg00@gmail.com>
2023-01-21 18:24:16 +0600

    changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
-// middleware
+// middlewares
 app.use(cors());
 app.use(express.json());
 
